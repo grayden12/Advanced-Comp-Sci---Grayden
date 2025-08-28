35988b3519c0550e0177bd9343470b377e816d15
grayden12 <glichtman1@hwemail.com>
2025-08-28 13:29:15 -0700

    class for 8/28/25

---
+++ Day 0/Firehose.java
@@ -0,0 +1,37 @@
+public class Firehose {
+
+    private String color;
+    private int cost;
+    private boolean inUse;
+    private double weight;
+    private int length;
+ 
+    public Firehose() {
+        color = "grey";
+        cost = 1200;
+        inUse = false;
+        weight = 25.5;
+        length = 100;
+    }
+    public void use(){
+        inUse = true;
+        System.out.println("Using");
+    }
+
+    public void paint(){
+        color = "Blue";
+        cost = cost + 5;
+
+        if (cost > 1210) {
+            System.out.println("The cost is getting out ofhand");
+        }
+        System.out.println("Cost is:" + cost);
+        
+        
+    }
+
+    public void unwind(){
+        System.out.println("Un winded");
+    }
+
+}
---
+++ Day 0/FirehoseTester.java
@@ -0,0 +1,12 @@
+public class FirehoseTester {
+    public static void main() {
+        System.out.println("Yo, whaddup!");
+
+        Firehose hose1 = new Firehose();
+        hose1.paint();
+        hose1.unwind();
+        hose1.use();
+    }
+
+   
+}
